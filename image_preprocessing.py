7ebd1a5456265bb4c4a0eb32a173d4bd59672a36
saeedshoarayenejati <34568429+saeedshoarayenejati@users.noreply.github.com>
2019-03-11 20:47:23 -0400

    done

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -5,8 +5,11 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision
 import numpy as np
+import pandas as pd
+
 
 def preprocess(images_train, images_test):
+
     largestsize_train = []
     largestsize_test = []
 
@@ -46,9 +49,12 @@ def preprocess(images_train, images_test):
     return largestsize_train, largestsize_test
 
 
-x_train_images = pd.read_pickle('./train_images.pkl')
-x_test_images = pd.read_pickle('./test_images.pkl')
-y_train_labels = pd.read_csv('./train_labels.csv')
+x_train_images = pd.read_pickle(
+    'drive/My Drive/ML_miniproject_3/train_images.pkl')
+x_test_images = pd.read_pickle(
+    'drive/My Drive/ML_miniproject_3/test_images.pkl')
+y_train_labels = pd.read_csv(
+    'drive/My Drive/ML_miniproject_3//train_labels.csv')
 
 filtered_datatrain = x_train_images
 filtered_datatrain[filtered_datatrain < 240] = 0
@@ -62,3 +68,5 @@ filtered_datatest = filtered_datatest.reshape(-1, 64, 64)
 
 largestsize_train, largestsize_test = preprocess(
     filtered_datatrain, filtered_datatest)
+plt.imshow(train_images[4])
+plt.imshow(largestsize_train[5])
